bf5ed25eeb54b42fb19436884fd43bb3ab0321be
bjmhe <hi@bjmhe.me>
2026-06-01 01:56:03 +0800

    Refactor CLI commands in index.ts to consolidate fetching functionality into a single 'fetch' command, allowing users to select documentation types (FUNDING.yml, ISSUE_TEMPLATE, PULL_REQUEST_TEMPLATE) interactively, improving user experience and code maintainability.

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,35 @@
 import fs from "node:fs";
 import path from "node:path";
 
-import { intro, log } from "@clack/prompts";
+import { intro, log, select } from "@clack/prompts";
 import cac from "cac";
 
 import { version } from "../package.json";
 
 intro("Welcome to @bjmhe/bjmhe");
+
+const docsMap = {
+  fund: {
+    label: "FUNDING.yml",
+    urls: ["https://raw.githubusercontent.com/bjmhe/bjmhe/refs/heads/master/.github/FUNDING.yml"],
+    fileName: ".github",
+  },
+  issue: {
+    label: "ISSUE_TEMPLATE",
+    urls: [
+      "https://raw.githubusercontent.com/bjmhe/bjmhe/refs/heads/master/.github/ISSUE_TEMPLATE/bug_report.yml",
+      "https://raw.githubusercontent.com/bjmhe/bjmhe/refs/heads/master/.github/ISSUE_TEMPLATE/config.yml",
+      "https://raw.githubusercontent.com/bjmhe/bjmhe/refs/heads/master/.github/ISSUE_TEMPLATE/feature_request.yml",
+      "https://raw.githubusercontent.com/bjmhe/bjmhe/refs/heads/master/.github/ISSUE_TEMPLATE/typo.yml",
+    ],
+    fileName: ".github/ISSUE_TEMPLATE",
+  },
+  pull: {
+    label: "PULL_REQUEST_TEMPLATE",
+    urls: ["https://raw.githubusercontent.com/bjmhe/bjmhe/refs/heads/master/.github/PULL_REQUEST_TEMPLATE.md"],
+    fileName: ".github",
+  },
+}
 
 function writeFileEnsuringDir(filePath: string, content: string) {
   fs.mkdirSync(path.dirname(filePath), { recursive: true });
@@ -16,72 +39,36 @@ function writeFileEnsuringDir(filePath: string, content: string) {
 
 const cli = cac();
 
-cli.command("fund", "Fetch FUNDING.yml from @bjmhe").action(async () => {
-  log.info("Fetching FUNDING.yml from @bjmhe/bjmhe...");
-  try {
-    // 拉取 https://raw.githubusercontent.com/bjmhe/bjmhe/refs/heads/master/.github/FUNDING.yml 到本地的 .github/FUNDING.yml
-    const fundingUrl =
-      "https://raw.githubusercontent.com/bjmhe/bjmhe/refs/heads/master/.github/FUNDING.yml";
-    const funding = await fetch(fundingUrl);
-    const fundingContent = await funding.text();
-    // 当前命令行目录
-    const currentDir = process.cwd();
-    writeFileEnsuringDir(path.join(currentDir, ".github/FUNDING.yml"), fundingContent);
-    log.success("FUNDING.yml fetched successfully");
-  } catch (error) {
-    log.error("Failed to fetch FUNDING.yml");
-    console.error(JSON.stringify(error, null, 2));
-  }
-});
-
-cli.command("issue", "Fetch ISSUE_TEMPLATE from @bjmhe").action(async () => {
-  log.info("Fetching ISSUE_TEMPLATE from @bjmhe/bjmhe...");
-  try {
-    const issueTemplates = [
-      "https://raw.githubusercontent.com/bjmhe/bjmhe/refs/heads/master/.github/ISSUE_TEMPLATE/bug_report.yml",
-      "https://raw.githubusercontent.com/bjmhe/bjmhe/refs/heads/master/.github/ISSUE_TEMPLATE/config.yml",
-      "https://raw.githubusercontent.com/bjmhe/bjmhe/refs/heads/master/.github/ISSUE_TEMPLATE/feature_request.yml",
-      "https://raw.githubusercontent.com/bjmhe/bjmhe/refs/heads/master/.github/ISSUE_TEMPLATE/typo.yml",
-    ];
-
-    for (const issueTemplate of issueTemplates) {
-      const issueTemplateResponse = await fetch(issueTemplate);
-      const issueTemplateContent = await issueTemplateResponse.text();
-      // 当前命令行目录
-      const currentDir = process.cwd();
-      writeFileEnsuringDir(
-        path.join(currentDir, ".github/ISSUE_TEMPLATE", issueTemplate.split("/").pop()!),
-        issueTemplateContent,
-      );
+cli.command("fetch", "Fetch docs from @bjmhe")
+  .action(async () => {
+    const type = await select({
+      message: 'Pick a docs type.',
+      // options: [
+      //   { value: 'fund', label: 'FUNDING.yml' },
+      //   { value: 'issue', label: 'ISSUE_TEMPLATE' },
+      //   { value: 'pull', label: 'PULL_REQUEST_TEMPLATE' },
+      // ],
+      options: Object.entries(docsMap).map(([value, item]) => ({
+        value,
+        label: item.label,
+      })),
+    });
+    if (!type) {
+      log.error("No docs type selected");
+      return;
     }
-    log.success("ISSUE_TEMPLATE fetched successfully");
-  } catch (error) {
-    log.error("Failed to fetch ISSUE_TEMPLATE");
-    console.error(JSON.stringify(error, null, 2));
-  }
-});
-
-cli.command("pull", "Fetch PULL_REQUEST_TEMPLATE from @bjmhe").action(async () => {
-  log.info("Fetching PULL_REQUEST_TEMPLATE from @bjmhe/bjmhe...");
-  try {
-    const issueTemplates = ["https://raw.githubusercontent.com/bjmhe/bjmhe/refs/heads/master/.github/PULL_REQUEST_TEMPLATE.md"];
-
-    for (const issueTemplate of issueTemplates) {
-      const issueTemplateResponse = await fetch(issueTemplate);
-      const issueTemplateContent = await issueTemplateResponse.text();
-      // 当前命令行目录
-      const currentDir = process.cwd();
-      writeFileEnsuringDir(
-        path.join(currentDir, ".github", issueTemplate.split("/").pop()!),
-        issueTemplateContent,
-      );
+    const docs = docsMap[type as keyof typeof docsMap];
+    if (!docs) {
+      log.error("No docs found");
+      return;
     }
-    log.success("PULL_REQUEST_TEMPLATE fetched successfully");
-  } catch (error) {
-    log.error("Failed to fetch PULL_REQUEST_TEMPLATE");
-    console.error(JSON.stringify(error, null, 2));
-  }
-});
+    for (const url of docs.urls) {
+      const response = await fetch(url);
+      const content = await response.text();
+      writeFileEnsuringDir(path.join(process.cwd(), docs.fileName, url.split("/").pop()!), content);
+    }
+    log.success("Docs fetched successfully");
+  })
 
 cli.help();
 cli.version(version);
