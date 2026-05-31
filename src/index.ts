5cb523005da1382933fc3ab9c1f4398a2bcb5342
bjmhe <hi@bjmhe.me>
2026-06-01 01:40:23 +0800

    Add @clack/prompts as a dependency in package.json and pnpm-lock.yaml, and integrate prompts in index.ts for enhanced user interaction in the CLI.

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@
 import cac from "cac";
 import fs from "node:fs";
 import path from "node:path";
+import { intro, log } from '@clack/prompts';
 import { version } from "../package.json";
+
+intro("Welcome to @bjmhe/bjmhe");
 
 const cli = cac();
 
 cli.command("fund", "Fetch FUNDING.yml from @bjmhe")
   .action(async () => {
+    log.info("Fetching FUNDING.yml from @bjmhe/bjmhe...");
     // 拉取 https://raw.githubusercontent.com/bjmhe/bjmhe/refs/heads/master/.github/FUNDING.yml 到本地的 .github/FUNDING.yml
     const fundingUrl = "https://raw.githubusercontent.com/bjmhe/bjmhe/refs/heads/master/.github/FUNDING.yml";
     const funding = await fetch(fundingUrl);
@@ -15,6 +19,7 @@ cli.command("fund", "Fetch FUNDING.yml from @bjmhe")
     // 当前命令行目录
     const currentDir = process.cwd();
     fs.writeFileSync(path.join(currentDir, ".github/FUNDING.yml"), fundingContent);
+    log.success("FUNDING.yml fetched successfully");
   })
 
 cli.help();
