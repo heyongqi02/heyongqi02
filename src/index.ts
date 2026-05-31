161c7622cc8483997d562bcd224fe636df881ea1
bjmhe <hi@bjmhe.me>
2026-06-01 01:31:00 +0800

    Refactor CLI options in index.ts by removing the type option and updating the version display to use the package version from package.json.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,15 @@
 #!/usr/bin/env node
 import cac from "cac";
+import { version } from "../package.json";
 
 const cli = cac();
 
-cli.option("--type [type]", "Choose a project type", {
-  default: "node",
-});
-cli.option("--name <name>", "Provide your name");
+cli.command("lint [...files]", "Lint files")
+  .action((files, options) => {
+    console.log(files, options);
+  })
 
-cli.command("lint [...files]", "Lint files").action((files, options) => {
-  console.log(files, options);
-});
-
-// Display help message when `-h` or `--help` appears
 cli.help();
-// Display version number when `-v` or `--version` appears
-// It's also used in help message
-cli.version("0.0.0");
+cli.version(version);
 
 cli.parse();
